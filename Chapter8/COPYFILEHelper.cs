5670b6f779f2a324be6b96a7dfed54617f4afa07
wuke1988 <470041973@qq.com>
2017-06-26 20:53:29 +0800

    8章:流和序列化

---
+++ Chapter8/COPYFILEHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter8
+{
+    /// <summary>
+    /// 基础流：文件流
+    /// </summary>
+    class COPYFILEHelper
+    {
+
+        public void CopyFile(string sourcePath,string outPath)
+        {
+            int bufferSize = 10240;
+
+            //读入文件流
+            Stream source = new FileStream(sourcePath, FileMode.Open,FileAccess.Read);
+            //输出文件流
+            Stream target = new FileStream(outPath,FileMode.OpenOrCreate,FileAccess.Write);
+
+            byte[] buffer = new byte[bufferSize];
+
+            int bytesRead = 0;
+
+            do
+            {
+                bytesRead = source.Read(buffer, 0, buffer.Length);
+                target.Write(buffer, 0, buffer.Length);
+
+            } while (bytesRead > 0);
+
+            source.Dispose();
+            target.Dispose();
+        }
+
+        public void Test()
+        {
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            CopyFile("log636339153387509518.log", DateTime.Now.Ticks + "copyfile.log");
+            watch.Stop();
+
+            Console.WriteLine("耗时:" + watch.ElapsedMilliseconds);
+        }  
+    }
+}
---
+++ Chapter8/CompressionHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter8
+{
+    class CompressionHelper
+    {
+        public void CompressionFile(string sourcePath,string outPath)
+        {
+            using (FileStream source = new FileStream(sourcePath, FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream compressedFileStream = new FileStream(outPath,FileMode.Create,FileAccess.Write))
+                {
+                    using (DeflateStream deflate = new DeflateStream(compressedFileStream, CompressionMode.Compress, true))
+                    {
+                        source.CopyTo(deflate);
+
+                    }
+                }
+            }
+        }
+        
+
+        public void Test()
+        {
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            CompressionFile("log636339153387509518.log", DateTime.Now.Ticks + "Compressd.log");
+            watch.Stop();
+
+            Console.WriteLine("耗时:" + watch.ElapsedMilliseconds);
+        }
+    }
+}
