2bebd7570e82450c0aa58831743c470987cd726c
wuke1988 <470041973@qq.com>
2017-06-28 20:32:21 +0800

    添加部分序列化代码

---
+++ Chapter8/ObjectSerialization.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter8
+{
+    public class ObjectSerialization
+    {
+        public void Test()
+        {
+            Product item = new Product(12398) {  Name="小米6"};
+            using (FileStream outstream = new FileStream("Product.obj", FileMode.Create, FileAccess.Write))
+            {
+                IFormatter formatter = new BinaryFormatter();
+
+                formatter.Serialize( outstream,item);
+                
+            }                          
+        }
+    }
+    [Serializable]
+    public class Product:IDeserializationCallback
+    {
+        private int id;
+
+        
+        public string Name { get; set; }
+
+        public Product(int id)
+        {
+            this.id = id;
+        }
+        public Product()
+        {
+
+        }
+        [OnSerialized]
+        public void OnSerializing()
+        {
+
+        }
+        [OnDeserialized]
+        public void OnDeserialized()
+        {
+
+        }
+
+        public void OnDeserialization(object sender)
+        {
+            this.id = 10010;
+        }
+    }
+}
