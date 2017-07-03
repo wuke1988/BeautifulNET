74e2ca5d287783b3cb55b5d14024b40c3f3e5e98
wuke1988 <470041973@qq.com>
2017-07-03 19:52:01 +0800

    完成12章反射

---
+++ Chapter12/CustomAttribute.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter12
+{
+    /// <summary>
+    /// 声明一个说明特性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Struct|AttributeTargets.Enum|AttributeTargets.Constructor|AttributeTargets.Method|AttributeTargets.Property|AttributeTargets.Field
+        |AttributeTargets.Event|AttributeTargets.Interface|AttributeTargets.Delegate,Inherited =false,AllowMultiple =true)]
+    [Serializable]
+    public class RecordAttribute:Attribute
+    {
+        public RecordAttribute(string recordType,string author,string date)
+        {
+            this.RecordType = recordType;
+            this.Author = author;
+            this.Date = Convert.ToDateTime(date);
+        }
+        //位置属性
+        public string RecordType { get; }
+        //位置属性
+        public string Author { get; }
+        //位置属性
+        public DateTime Date { get; }
+        //命名属性
+        public string MeMo
+        { get; set; }
+
+        public override string ToString()
+        {
+            return RecordType+ Author+ Date;
+        }
+    }
+    [Record("Chapter12.Test","wuke","2017-7-3")]
+    public class Test
+    {
+        [Record("Chapter12.Test.Method","wuke", "2017-7-3")]
+        public void Method()
+        {
+            Console.WriteLine("BlaBla...");
+        }
+
+    }
+
+    public class Demo2
+    {
+        public void Test()
+        {
+            Type t = typeof(Test);
+
+            //反射获取所有的用户自定义特性声明
+            object[] objs = t.GetCustomAttributes(typeof(RecordAttribute),false);
+
+            foreach (object obj in objs)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter12/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type t = typeof(BookStatue);
+            //foreach (FieldInfo field in t.GetFields())
+            //{
+            //    if(!field.IsSpecialName)
+            //        Console.WriteLine( field.Name+" "+field.GetRawConstantValue());
+            //}
+
+
+            //Type type = typeof(Demo);
+            //MethodInfo[] methods = type.GetMethods();
+            //foreach (MethodInfo method in methods)
+            //    Console.WriteLine(method.Name);
+
+            //PropertyInfo[] propertys = type.GetProperties();
+            //foreach (PropertyInfo property in propertys)
+            //    Console.WriteLine(property.Name);
+
+
+            Demo2 demo2 = new Demo2();
+            demo2.Test();
+
+            Console.ReadLine();
+        }
+    }
+
+    public enum BookStatue
+    {
+        未提交,
+        已提交,
+        已取消,
+        已订阅
+    }
+
+    public delegate void Notify(string name);
+    public class Demo 
+    {
+        public String Name { get; set; }
+
+        public event Notify NotifyEvent;
+
+        public Demo()
+        {
+            NotifyEvent += fun1;
+        }
+
+        public void fun1(string str)
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            //判断两个对象的类型是否相同
+            Type type = obj.GetType();
+            Type type2 = this.GetType();
+
+            if (type != type2)
+                return false;
+
+            //判断两个对象是否能按位比较
+            Object a = this;
+
+            if (CanCompareBits(a))
+                return FastEqualsCheck(a,obj);
+
+            //反射字段比较两个对象
+            FieldInfo[] fields = type.GetFields(BindingFlags.NonPublic|BindingFlags.Public|BindingFlags.Instance);
+
+            foreach (FieldInfo field in fields)
+            {
+                object obj3 = field.GetValue(obj);
+                object obj4 = field.GetValue(a);
+
+                if (obj3 == null)
+                {
+                    if (obj4 == null)
+                        return true;
+                    else
+                        return false;
+                }
+                else
+                {
+                    if (obj3.Equals(obj4))
+                        return true;
+                    return false;
+                }
+            }
+            return false;
+        }
+        public bool CanCompareBits(Object a)
+        {
+            return false;
+        }
+        public bool FastEqualsCheck(Object a,Object b)
+        {
+            return false;
+        }
+    }
+}
